c8f4b6a92c74bbf590878dad6d82ba9ec1f739b8
Lucas J. Cunha <lucasgb7_ks@hotmail.com>
2020-10-11 13:46:59 -0300

    rgb server dando errado

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,39 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
-
-// Load Wi-Fi library
 #include <WiFi.h>
+#include <analogWrite.h>
 
-// Replace with your network credentials
+// Definindo as credenciais da rede Wi-Fi
 const char* ssid = "Assembly";
 const char* password = "#code1304";
 
-// Set web server port number to 80
+// Define a porta do server como 80
 WiFiServer server(80);
 
-// Variable to store the HTTP request
+// Variavel para armazenar o HTTP request
 String header;
 
 // Auxiliar variables to store the current output state
-String blueState = "off";
-String greenState = "off";
-String redState = "off";
+String redString = "0";
+String greenString = "0";
+String blueString = "0";
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int pos4 = 0;
 int lightValue, lightInit;
 String color;
 
 // Define as saídas para seus pinos GPIO
 const int photoresistor = 35;
-const int blue = 25;
-const int green = 26;
-const int red = 27;
+const int bluePin = 25;
+const int greenPin = 26;
+const int redPin = 27;
+
+// Configurações PWM
+const int freq = 5000;
+const int redChannel = 0;
+const int greenChannel = 1;
+const int blueChannel = 2;
+const int resolution = 8;
 
 // Current time
 unsigned long currentTime = millis();
@@ -38,47 +44,52 @@ const long timeoutTime = 2000;
 
 //Funções responsáveis por executar o brilho selecionado
 void redColor(){
-  digitalWrite(blue, LOW);
-  digitalWrite(green, LOW);
-  digitalWrite(red, HIGH);
+  digitalWrite(bluePin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(redPin, HIGH);
 }
 void blueColor(){
-  digitalWrite(blue, HIGH);
-  digitalWrite(green, LOW);
-  digitalWrite(red, LOW);
+  digitalWrite(bluePin, HIGH);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(redPin, LOW);
 }
 void greenColor(){
-  digitalWrite(blue, LOW);
-  digitalWrite(green, HIGH);
-  digitalWrite(red, LOW);
+  digitalWrite(bluePin, LOW);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(redPin, LOW);
 }
 void whiteColor(){
-  digitalWrite(blue, HIGH);
-  digitalWrite(green, HIGH);
-  digitalWrite(red, HIGH);
+  digitalWrite(bluePin, HIGH);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(redPin, HIGH);
 }
 void blackOut(){
-  digitalWrite(blue, LOW);
-  digitalWrite(green, LOW);
-  digitalWrite(red, LOW);
+  digitalWrite(bluePin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(redPin, LOW);
+}
+
+void RGB_color(int redValue, int greenValue, int blueValue){
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
 }
 
 void setup() {
   Serial.begin(115200);
-  // Initialize the output variables as outputs
+  // Inicaliza as variaveis de saida
   pinMode(photoresistor, OUTPUT);
-  pinMode(blue, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(red, OUTPUT);
-  // Set outputs to LOW
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  // Define as saidas desligadas
   digitalWrite(photoresistor, LOW);
-  digitalWrite(blue, LOW);
-  digitalWrite(green, LOW);
-  digitalWrite(red, LOW);
-
+  digitalWrite(bluePin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(redPin, LOW);
   // Começa a leitura do valor de luminosidade
   lightInit = analogRead(photoresistor);
-  // Connect to Wi-Fi network with SSID and password
+  // Conecta a rede Wi-Fi com o SSID e password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -86,7 +97,6 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
@@ -94,25 +104,28 @@ void setup() {
   server.begin();
 }
 
-void loop(){
-  // WiFiClient client = server.available();   // Listen for incoming clients
-  lightValue = analogRead(photoresistor);   // Leitura de luminosidade
-  if(lightValue - lightInit <  50){
+void loop()
+{
+  WiFiClient client = server.available();   // Servidor disponivel para acesso
+  // Leitura de Luminosidade
+  /*
+  lightValue = analogRead(photoresistor);
+  if(lightValue - lightInit <  50){         
       whiteColor();
   }else{
       blackOut();
   }
-/*
-  if (client) {                             // If a new client connects,
+  */
+  if (client) {                             // Caso haja alguma conexao
     currentTime = millis();
     previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // Enquanto o cliente ficar conectado
       currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+      if (client.available()) {             // Caso haver algum dado para ler
+        char c = client.read();
+        Serial.write(c);
         header += c;
         if (c == '\n') {                    // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
@@ -125,79 +138,34 @@ void loop(){
             client.println("Connection: close");
             client.println();
             
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /25/on") >= 0) {
-              Serial.println("GPIO 25 on");
-              blueState = "on";
-              digitalWrite(blue, HIGH);
-            } else if (header.indexOf("GET /25/off") >= 0) {
-              Serial.println("GPIO 25 off");
-              blueState = "off";
-              digitalWrite(blue, LOW);
-            } else if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              greenState = "on";
-              digitalWrite(green, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              greenState = "off";
-              digitalWrite(green, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              redState = "on";
-              digitalWrite(red, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              redState = "off";
-              digitalWrite(red, LOW);
-            }
-            
-            
-            // Display the HTML web page
+            // Pagina Web em HTML
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #FF0000; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #00FF00;}</style></head>");
-            client.println(".button3 {background-color: #0000FF;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 25 
-            client.println("<p>GPIO 25 - State " + blueState + "</p>");
-            // If the blueState is off, it displays the ON button       
-            if (blueState=="off") {
-              client.println("<p><a href=\"/25/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/25/off\"><button class=\"button button2 button3\">OFF</button></a></p>");
-            } 
-
-            // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>GPIO 26 - State " + greenState + "</p>");
-            // If the greenState is off, it displays the ON button       
-            if (greenState=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2 button3\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>GPIO 27 - State " + redState + "</p>");
-            // If the redState is off, it displays the ON button       
-            if (redState=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2 button3\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
+            client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
+            client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
+            client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1></div>");
+            client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Change Color</a> ");
+            client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
+            client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
+            client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"&\";}</script></body></html>");
             // The HTTP response ends with another blank line
             client.println();
+
+            // Request sample: /?r201g32b255&
+            // Red = 201 | Green = 32 | Blue = 255
+            if(header.indexOf("GET /?r") >= 0) {
+              pos1 = header.indexOf('r');
+              pos2 = header.indexOf('g');
+              pos3 = header.indexOf('b');
+              pos4 = header.indexOf('&');
+              redString = header.substring(pos1+1, pos2);
+              greenString = header.substring(pos2+1, pos3);
+              blueString = header.substring(pos3+1, pos4);
+              /*Serial.println(redString.toInt());
+              Serial.println(greenString.toInt());
+              Serial.println(blueString.toInt());*/
+              RGB_color(redString.toInt(), greenString.toInt(), blueString.toInt());
             // Break out of the while loop
             break;
           } else { // if you got a newline, then clear currentLine
@@ -208,11 +176,12 @@ void loop(){
         }
       }
     }
-    // Clear the header variable
+    // Limpa o cabecalho
     header = "";
-    // Close the connection
+    // Encerra a conexao
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
-  }*/
+    }
+  }
 }
