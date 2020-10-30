8058c7d86e35ee72ea85ee12cb66098e95868bd6
JonathWesley <jonathherdt@gmail.com>
2020-10-30 15:34:27 -0300

    slider

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ String header;
 // Variaveis auxiliares para fotoresistor e estado do botao de ligado/desligado
 int lightValue, lightInit, distance;
 bool powerButtonState = false;
-bool ledState = false;
+bool ledState = true;
 long duration;
 
 // Define as saídas para seus pinos GPIO
@@ -34,77 +34,66 @@ unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
 //Funções responsáveis por definir as cores do led RGB
-void redColor()
-{
+void redColor(){
   digitalWrite(bluePin, LOW);
   digitalWrite(greenPin, LOW);
   digitalWrite(redPin, HIGH);
 }
-void blueColor()
-{
+void blueColor(){
   digitalWrite(bluePin, HIGH);
   digitalWrite(greenPin, LOW);
   digitalWrite(redPin, LOW);
 }
-void greenColor()
-{
+void greenColor(){
   digitalWrite(bluePin, LOW);
   digitalWrite(greenPin, HIGH);
   digitalWrite(redPin, LOW);
 }
-void whiteColor()
-{
+void whiteColor(){
   digitalWrite(bluePin, HIGH);
   digitalWrite(greenPin, HIGH);
   digitalWrite(redPin, HIGH);
 }
-void yellowColor()
-{
+void yellowColor(){
   digitalWrite(bluePin, LOW);
   digitalWrite(greenPin, HIGH);
   digitalWrite(redPin, HIGH);
 }
-void magentaColor()
-{
+void magentaColor(){
   digitalWrite(bluePin, HIGH);
   digitalWrite(greenPin, LOW);
   digitalWrite(redPin, HIGH);
 }
-void cyanColor()
-{
+void cyanColor(){
   digitalWrite(bluePin, HIGH);
   digitalWrite(greenPin, HIGH);
   digitalWrite(redPin, LOW);
 }
-void blackOut()
-{
+void blackOut(){
   digitalWrite(bluePin, LOW);
   digitalWrite(greenPin, LOW);
   digitalWrite(redPin, LOW);
 }
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   // Inializa as variaveis de saida e entrada
-  pinMode(photoresistor, OUTPUT);
+  //pinMode(photoresistor, INPUT);
   pinMode(bluePin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(redPin, OUTPUT);
-  pinMode(redPin, INPUT);
   // Define as saidas desligadas
-  digitalWrite(photoresistor, LOW);
+  //digitalWrite(photoresistor, LOW);
   digitalWrite(bluePin, LOW);
   digitalWrite(greenPin, LOW);
   digitalWrite(redPin, LOW);
   // Começa a leitura do valor de luminosidade
-  lightInit = analogRead(photoresistor);
+  //lightInit = analogRead(photoresistor);
   // Conecta a rede Wi-Fi com o SSID e password
   Serial.print("Conectando a rede ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
@@ -115,22 +104,19 @@ void setup()
   server.begin();
 }
 
-void loop()
-{
+void loop(){
 
   WiFiClient client = server.available(); // Servidor disponivel para acesso
   // Leitura de luminosidade
   lightValue = analogRead(photoresistor);
-  if (lightValue - lightInit < 50)
-  {
+  Serial.println(lightValue);
+  /*if (lightValue - lightInit < 50){
     whiteColor();
   }
-  else
-  {
+  else{
     blackOut();
-  }
-  if (client)
-  { // Caso haja alguma conexao
+  }*/
+  if (client){ // Caso haja alguma conexao
     currentTime = millis();
     previousTime = currentTime;
     Serial.println("Cliente novo.");
@@ -198,9 +184,9 @@ void loop()
             client.println(".button6 { background-color: #0000FF; border: 2px solid #000000;; color: black; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }");
             client.println(".button7 { background-color: #FF00FF; border: 2px solid #000000;; color: black; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }");
             client.println(".slidecontainer { width: 100%; }");
-            client.println(".slider { -webkit-appearance: none; width: 100%; height: 10px; border-radius: 5px; background: #d3d3d3; outline: none; opacity: 0.7; -webkit-transition: .2s; transition: opacity .2s; }");
-            client.println(".slider::-webkit-slider-thumb { -webkit-appearance: none; appearance: none; width: 23px; height: 24px; border: 0; background: \"contrasticon.png\"; cursor: pointer; }");
-            client.println(".slider::-moz-range-thumb { width: 23px; height: 24px; border: 0; background: \"contrasticon.png\"; cursor: pointer; }");
+            client.println(".slider {-webkit-appearance: none;width: 100%;height: 15px;border-radius: 5px;background: #d3d3d3;outline: none;opacity: 0.7;-webkit-transition: .2s;transition: opacity .2s;}");
+            client.println(".slider::-webkit-slider-thumb {-webkit-appearance: none;appearance: none;width: 25px;height: 25px;border-radius: 50%;background: #333;cursor: pointer;}");
+            client.println(".slider::-moz-range-thumb {width: 25px;height: 25px;border-radius: 50%;background: #333;cursor: pointer;}");
 
             // Cabeçalho da pagina WEB
             client.println("</style></head>");
@@ -211,7 +197,7 @@ void loop()
             client.println("<form id=\"myForm\"><center>");
             client.println("<h3>Ligar/Desligar</h3>");
             client.println("<div class=\"btn-group\">");
-            client.println("<button class=\"buttonPower\" name=\"power\" value=\"on/off\" type=\"submit\"><img src=\"powerButton.png\"></button>");
+            client.println("<button class=\"buttonPower\" name=\"power\" value=\"onoff\" type=\"submit\"><img src=\"powerButton.png\"></button>");
             client.println("</div><br>");
             // Slider de Brilho
             client.println("<h3>Brilho</h3>");
@@ -233,7 +219,7 @@ void loop()
 
             // Script
             client.println("<script>");
-            client.println("var slider = document.getElementById(\"myRange\"); slider.onmouseup = function() { document.getElementById(\"myForm\").submit(); } slider.ontouchend = function() { document.getElementById(\"myForm\").submit(); }");
+            client.println("var slider = document.getElementById(\"myRange\"); slider.onmouseup = function() { document.getElementById(\"myForm\").submit(); }; slider.ontouchend = function() { document.getElementById(\"myForm\").submit(); };");
             client.println("</script></body></html>");
             break;
           }
